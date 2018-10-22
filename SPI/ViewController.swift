69208ddb53cac24ecf280c322c576e93d915ce56
Edson Trejo <edsontrejo@edsons-macbook-pro.local>
2018-10-21 23:25:04 -0500

    Initial Commit

---
+++ SPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SPI
+//
+//  Created by Edson Trejo on 10/21/18.
+//  Copyright © 2018 Edson Trejo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
